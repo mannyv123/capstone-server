30e59570f1aab1608c4cff0c45f148b651a3a9bd
Manjot Virdi <virdi_manjot@hotmail.com>
2023-04-03 12:09:32 -0700

    add and test route for creating new user

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -2,3 +2,5 @@ const router = require("express").Router();
 const usersController = require("../controllers/usersController");
 
 router.route("/").post(usersController.createUser);
+
+module.exports = router;
