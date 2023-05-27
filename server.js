1799b9d8dba1d14aa90023ff8c9de0c79b694e08
Manjot Virdi <virdi_manjot@hotmail.com>
2023-05-27 16:15:07 -0700

    add cors origin header

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5001;
 
 //Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://collections-capstone.netlify.app",
+    })
+);
 app.use(express.static("public")); //allows access to the public folder, where video images are stored
 
 const usersRoute = require("./routes/usersRoute"); //route for user related endpoints
